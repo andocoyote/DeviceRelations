9fe493f7d22f04b79f1e542217bb47ff46193d33
Andrew Mikesell <andocoyote@outlook.com>
2017-04-03 14:53:37 -0700

    Adding ability to query for all related devices when given a device ID

--- main.cpp
+++ main.cpp
@@ -1,8 +1,77 @@
 #include <windows.h>
 #include <iostream>
+#include <Cfgmgr32.h>
+#include <string.h>
 
-DWORD wmain(DWORD argc, PWSTR *argv[])
+using namespace std;
+
+DWORD wmain(DWORD argc, PCWSTR argv[])
 {
+	DWORD returnCode = 0;
+	ULONG bufferSize = 0;
+	PCWSTR deviceID = nullptr;
+	PWSTR relatedDevicesBuffer = nullptr;
 	
+	if(argc != 2)
+	{
+		wcout << "Call this tool with the device ID supplied on the command line.\n";
+		goto Cleanup;
+	}
+	
+	deviceID = argv[1];
+	
+	wcout << "DeviceID: " << deviceID << endl;
+	
+	// Get the size for the buffer of device IDs
+	returnCode = CM_Get_Device_ID_List_SizeW(
+		  &bufferSize,
+		  deviceID,
+		  CM_GETIDLIST_FILTER_NONE
+		);
+		
+	if(returnCode != CR_SUCCESS)
+	{
+		wcout << "Error: CM_Get_Device_ID_List_Size returned 0x" << returnCode << endl;
+		goto Cleanup;
+	}
+	
+	if(bufferSize == 0)
+	{
+		wcout << "Error: Required buffer size returned by CM_Get_Device_ID_List_Size is 0\n";
+		goto Cleanup;
+	}
+	
+	wcout << "Required buffer size returned by CM_Get_Device_ID_List_Size is " << bufferSize << endl;
+	
+	relatedDevicesBuffer = new WCHAR[bufferSize];
+	
+	// Query for the list of related devices
+	returnCode = CM_Get_Device_ID_ListW(
+		  deviceID,
+		  relatedDevicesBuffer,
+		  bufferSize,
+		  CM_GETIDLIST_FILTER_NONE
+		);
+		
+	if(returnCode != CR_SUCCESS)
+	{
+		wcout << "Error: CM_Get_Device_ID_List returned 0x" << returnCode << endl;
+		goto Cleanup;
+	}
+	
+	// Iterate through the collection of related devices.
+	// The extra null terminator at the end of the collection allows us to stop the loop at *curDevice
+	for(LPWSTR curDevice = relatedDevicesBuffer; *curDevice; curDevice += wcslen(curDevice) + 1)
+    {
+		wcout << "Related devices: " << curDevice << endl;
+	}
+	
+Cleanup:
+
+	if(relatedDevicesBuffer)
+	{
+		delete [] relatedDevicesBuffer;
+	}
+
 	return 0;
 }
--- makefile
+++ makefile
@@ -2,15 +2,15 @@
 INCS=\
 
 # Specify the location to my SDK libs (e.g. DbgHelp.Lib)
-LIBS=\
-	"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib" 
+LIBS="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Lib";"c:\Program Files (x86)\Windows Kits\10\Lib\10.0.14393.0\um\x86" 
 	
 CC=cl
-CFLAGS=/c /EHsc
-#LDFLAGS=/link /LIBPATH:$(LIBS)
+CFLAGS=/c /EHsc /Od /MTd
+#CFLAGS=/c /EHsc /Od
+LDFLAGS=/link /LIBPATH:$(LIBS) /verbose:lib Cfgmgr32.lib /debug
 SOURCES=main.cpp
 OBJECTS=$(SOURCES:.cpp=.obj)
-EXECUTABLE=main
+EXECUTABLE=main.exe
 
 #Uncomment the following to enable debugging
 DBGFLAGS=/Zi
